87aabc8b1a85b9c3b4f55869b75d2c475ee1e337
Muhammad Mulla <muhammad.mulla@muhammads-air.home>
2024-07-11 15:24:00 +0100

    Unit Testing

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -1,3 +1,5 @@
+package Week2.Wednesday
+
 class TaxCalculator {
 
   // Tax bands (simplified to make testing a bit easier)
@@ -13,18 +15,29 @@ class TaxCalculator {
 
   // A method to calculate the total amount of tax to be paid, returned as a double
   def calculateTax(income: Double): Double = {
-    ???
+    if (income <= personalAllowance) 0
+    else if (income <= basicRateLimit) (income - personalAllowance) * basicRate
+    else if (income <= higherRateLimit) {
+      (basicRateLimit - personalAllowance) * basicRate + (income - basicRateLimit) * higherRate
+    }
+    else {
+      (basicRateLimit - personalAllowance) * basicRate +
+        (higherRateLimit - basicRateLimit) * higherRate +
+        (income - higherRateLimit) * additionalRate
+    }
   }
 
   // A method which can tell you if someone is a higher rate taxpayer
   def isHigherRateTaxpayer(income: Double): Boolean = {
-    ???
+    income > basicRateLimit
   }
 
   // A method that will return a string with the income limit of their current tax band.
   // The return will also be formatted, E.g: "£12,500" or "No limit"
   def formattedCurrentTaxAllowance(income: Double): String = {
-    ???
+    if (income <= personalAllowance) "£10,000"
+    else if (income <= basicRateLimit) "£50,000"
+    else if (income <= higherRateLimit) "£125,000"
+    else "No limit"
   }
-
 }
--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -1,18 +1,71 @@
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
+import Week2.Wednesday.TaxCalculator
 
 class TaxCalculatorSpec extends AnyWordSpec {
 
   val taxCalculator: TaxCalculator = new TaxCalculator
 
-  // I've done the first test for you!
   "TaxCalculator.calculateTax" should {
     "return the total amount of tax to pay" when {
       "the income is below the personal tax limit" in {
         val result: Double = taxCalculator.calculateTax(5000)
-
         assert(result == 0)
       }
+
+      "the income is exactly at the personal tax limit" in {
+        val result: Double = taxCalculator.calculateTax(10000)
+        assert(result == 0)
+      }
+
+      "the income is within the basic rate limit" in {
+        val result: Double = taxCalculator.calculateTax(30000)
+        assert(result == 4000) // (30000 - 10000) * 0.2
+      }
+
+      "the income is within the higher rate limit" in {
+        val result: Double = taxCalculator.calculateTax(100000)
+        assert(result == 28000) // Corrected expected value
+      }
+
+      "the income is within the additional rate limit" in {
+        val result: Double = taxCalculator.calculateTax(150000)
+        assert(result == 49250) // Corrected expected value
+      }
+    }
+  }
+
+  "TaxCalculator.isHigherRateTaxpayer" should {
+    "return true if income is within the higher rate limit" in {
+      val result: Boolean = taxCalculator.isHigherRateTaxpayer(100000)
+      assert(result)
+    }
+
+    "return false if income is within the basic rate limit" in {
+      val result: Boolean = taxCalculator.isHigherRateTaxpayer(30000)
+      assert(!result)
+    }
+  }
+
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return formatted string for personal allowance" in {
+      val result: String = taxCalculator.formattedCurrentTaxAllowance(5000)
+      assert(result == "£10,000")
+    }
+
+    "return formatted string for basic rate limit" in {
+      val result: String = taxCalculator.formattedCurrentTaxAllowance(30000)
+      assert(result == "£50,000")
+    }
+
+    "return formatted string for higher rate limit" in {
+      val result: String = taxCalculator.formattedCurrentTaxAllowance(100000)
+      assert(result == "£125,000")
+    }
+
+    "return 'No limit' for additional rate" in {
+      val result: String = taxCalculator.formattedCurrentTaxAllowance(150000)
+      assert(result == "No limit")
     }
   }
 }
